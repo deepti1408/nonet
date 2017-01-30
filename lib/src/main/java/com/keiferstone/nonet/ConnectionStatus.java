0cdae5c4a5ce5a24ec00f9e6a65ae4aad419aa09
Keifer Stone <keifer.stone@bottlerocketstudios.com>
2017-01-29 22:10:19 -0600

    added release signing. fixed javadocs

--- lib/src/main/java/com/keiferstone/nonet/ConnectionStatus.java
+++ lib/src/main/java/com/keiferstone/nonet/ConnectionStatus.java
@@ -3,8 +3,12 @@ package com.keiferstone.nonet;
 
 import android.support.annotation.IntDef;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 import static com.keiferstone.nonet.ConnectionStatus.*;
 
+@Retention(RetentionPolicy.SOURCE)
 @IntDef({CONNECTED, DISCONNECTED, UNKNOWN})
 public @interface ConnectionStatus {
     int CONNECTED = 100;
