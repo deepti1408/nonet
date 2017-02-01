c49b138e63adf21ee01b6bcc841c3d217ebd8194
Keifer Stone <keifer.stone@bottlerocketstudios.com>
2017-01-31 20:00:18 -0600

    started working unit tests

---
+++ nonet/src/test/java/com/keiferstone/nonet/NoNetTests.java
@@ -0,0 +1,14 @@
+package com.keiferstone.nonet;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NoNetTests {
+    @Test
+    public void defaultMonitorTest() throws Exception {
+        Monitor monitor = NoNet.monitor(null).start();
+        assertNotNull(monitor);
+        assertNotNull(monitor.observe());
+    }
+}
