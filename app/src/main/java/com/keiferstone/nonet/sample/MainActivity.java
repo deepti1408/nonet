5c3a1d7047dcbd552d6e53ad77e6c9a02c5fdb5a
Keifer Stone <keifer.stone@bottlerocketstudios.com>
2017-02-21 00:14:17 -0600

    refactored nonet to manage it's own activity lifecycle

--- app/src/main/java/com/keiferstone/nonet/sample/MainActivity.java
+++ app/src/main/java/com/keiferstone/nonet/sample/MainActivity.java
@@ -1,7 +1,11 @@
 package com.keiferstone.nonet.sample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.keiferstone.nonet.ConnectionStatus;
@@ -16,11 +20,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+        Button button = (Button) findViewById(R.id.new_activity_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+            }
+        });
 
         NoNet.monitor(this)
                 .poll()
@@ -34,19 +41,17 @@ public class MainActivity extends AppCompatActivity {
                         monitorCallback.onConnectionEvent(integer);
                     }
                 });
-    }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        NoNet.stopMonitoring();
+        NoNet.check(this)
+                .toast()
+                .start();
     }
 
     private Monitor.Callback monitorCallback = new Monitor.Callback() {
         @Override
         public void onConnectionEvent(@ConnectionStatus int connectionStatus) {
-            Toast.makeText(MainActivity.this, "[onConnectionEvent( " + connectionStatus + " )]", Toast.LENGTH_SHORT).show();
+            Log.d("K", "[onConnectionEvent( " + connectionStatus + " )]");
+            //Toast.makeText(MainActivity.this, "[onConnectionEvent( " + connectionStatus + " )]", Toast.LENGTH_SHORT).show();
         }
     };
 }
